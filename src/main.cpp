ee7835ee965fa6e17f7518d288d0259e8cb0efc7
amocualg <apavlov@bestplace.pro>
2024-11-27 19:38:03 +0300

    changed beh of panel button

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "raylib.h"
+#include "tinyfiledialogs.h"
 #include <iostream>
 #include <string>
 
@@ -21,6 +22,8 @@ int main(void) {
   int button_x = button_offset;
   int button_y = button_offset;
   std::string button_icon = ">";
+  CLITERAL(Color) button_color = BLACK;
+  CLITERAL(Color) button_text_color = RAYWHITE;
   bool panel_visible = false;
   bool is_animating = false;
 
@@ -28,6 +31,10 @@ int main(void) {
   const int exit_button_height = 40;
   const int exit_button_y = screen_height - exit_button_height - button_offset;
 
+  const int top_button_width = panel_width - 2 * button_offset;
+  const int top_button_height = 40;
+  const int top_button_x = button_offset;
+  const int top_button_y = button_offset + button_height + 10;
   SetTargetFPS(60);
 
   while (!WindowShouldClose()) {
@@ -37,6 +44,7 @@ int main(void) {
       button_x = button_offset;
 
     int exit_button_x = panel_x + button_offset;
+    int actual_top_button_x = panel_x + top_button_x;
 
     if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
       Vector2 mouse_position = GetMousePosition();
@@ -47,6 +55,9 @@ int main(void) {
         panel_visible = !panel_visible;
         is_animating = true;
         button_icon = panel_visible ? "<" : ">";
+        button_color = panel_visible ? RAYWHITE : BLACK;
+        button_text_color = panel_visible ? BLACK : RAYWHITE;
+
       }
       Rectangle exit_button_rect = {(float)exit_button_x, (float)exit_button_y,
                                     (float)exit_button_width,
@@ -54,6 +65,18 @@ int main(void) {
       if (CheckCollisionPointRec(mouse_position, exit_button_rect)) {
         CloseWindow();
         return 0;
+      }
+      Rectangle top_button_rect = {(float)actual_top_button_x,
+                                   (float)top_button_y, (float)top_button_width,
+                                   (float)top_button_height};
+      if (CheckCollisionPointRec(mouse_position, top_button_rect)) {
+        const char *filters[] = {"*.*"};
+        const char *file =
+            tinyfd_openFileDialog("Select a file", "", 0, filters, NULL, 0);
+        if (file) {
+          // TODO: implement file procces
+          std::cout << "Selected file: " << file << std::endl;
+        }
       }
     }
 
@@ -76,11 +99,20 @@ int main(void) {
     BeginDrawing();
     ClearBackground(RAYWHITE);
 
+    // panel and toggle button
     DrawRectangle(panel_x, 0, panel_width, screen_height, BLACK);
-    DrawRectangle(button_x, button_y, button_width, button_height, BLACK);
-    DrawText(button_icon.c_str(), button_x + 10, button_y + 5, 20, WHITE);
+    DrawRectangle(button_x, button_y, button_width, button_height, button_color);
+    DrawText(button_icon.c_str(), button_x + 10, button_y + 5, 20, button_text_color);
 
     if (panel_x > -panel_width + panel_animation_speed) {
+      // file button
+      DrawRectangle(actual_top_button_x, top_button_y, top_button_width,
+                    top_button_height, DARKGRAY);
+      DrawText("Open File",
+               actual_top_button_x +
+                   (top_button_width - MeasureText("Open File", 20)) / 2,
+               top_button_y + 10, 20, WHITE);
+      // exit button
       DrawRectangle(exit_button_x, exit_button_y, exit_button_width,
                     exit_button_height, RED);
       DrawText("EXIT",
@@ -89,8 +121,8 @@ int main(void) {
                exit_button_y + 10, 20, WHITE);
     }
 
-    DrawText("GUANDAO", (screen_width - MeasureText("GUANDAO", 40)) / 2,
-             screen_height / 2 - 20, 40, BLACK);
+    DrawText("GUANDAO", (screen_width - MeasureText("GUANDAO", 48)) / 2,
+             screen_height / 2 - 24, 40, BLACK);
     EndDrawing();
   }
 
