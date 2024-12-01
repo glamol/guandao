327c8d73cde91de6666123fbef87aac850ef59ef
Molphie <ei.pogr4work@gmail.com>
2024-12-01 20:22:28 +0300

    Added button class, exit button was fully refactored and it works

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,47 @@
 #include <iostream>
 #include <string>
 
+class Button{
+  
+  public:
+    //button
+    int x, y;
+    int width, height;
+    Color button_color = RAYWHITE;
+    //text
+    std::string text = "";
+    int text_font_size = 20;
+    Color text_color = BLACK;
+    
+    Button(int wid, int heig){
+      width = wid;
+      height = heig;
+    }
+    Button(int x, int y, int wid, int heig){
+      this->x = x;
+      this->y = y;
+      width = wid;
+      height = heig;
+    }
+    Button(int x, int y, int wid, int heig, std::string txt){
+      width = wid;
+      height = heig;
+      text = txt;
+    }
+    void draw(int mode){ // 0 - without text, 1 - centered text, 2 - ?? (may be added)
+      DrawRectangle(x, y, width, height, button_color);
+      //or change to switch/case construction
+      if (mode == 1) DrawText(text.c_str(), 
+                              x + (width - MeasureText(text.c_str(), 20)) / 2, 
+                              y + height / 4, 
+                              text_font_size, text_color);
+    }
+    bool is_clicked(Vector2 mouse_position){
+      Rectangle button_rectangle = {(float)x, (float)y, (float)width, (float)height};
+      return CheckCollisionPointRec(mouse_position, button_rectangle);
+    }
+};
+
 int main(void) {
   int monitor_num = 0;
   //  WARNING: GLFW: Failed to find selected monitor
@@ -27,9 +68,7 @@ int main(void) {
   bool panel_visible = false;
   bool is_animating = false;
 
-  const int exit_button_width = panel_width - 2 * button_offset;
-  const int exit_button_height = 40;
-  const int exit_button_y = screen_height - exit_button_height - button_offset;
+  Button exit_button(panel_width - 2 * button_offset, 40);
 
   const int top_button_width = panel_width - 2 * button_offset;
   const int top_button_height = 40;
@@ -43,7 +82,9 @@ int main(void) {
     else
       button_x = button_offset;
 
-    int exit_button_x = panel_x + button_offset;
+    exit_button.x = panel_x + button_offset;
+    exit_button.y = screen_height - exit_button.height - button_offset;
+    
     int actual_top_button_x = panel_x + top_button_x;
 
     if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
@@ -59,13 +100,11 @@ int main(void) {
         button_text_color = panel_visible ? BLACK : RAYWHITE;
 
       }
-      Rectangle exit_button_rect = {(float)exit_button_x, (float)exit_button_y,
-                                    (float)exit_button_width,
-                                    (float)exit_button_height};
-      if (CheckCollisionPointRec(mouse_position, exit_button_rect)) {
-        CloseWindow();
-        return 0;
+      if (exit_button.is_clicked(mouse_position)) {
+         CloseWindow(); 
+         return 0;
       }
+      
       Rectangle top_button_rect = {(float)actual_top_button_x,
                                    (float)top_button_y, (float)top_button_width,
                                    (float)top_button_height};
@@ -101,6 +140,7 @@ int main(void) {
 
     // panel and toggle button
     DrawRectangle(panel_x, 0, panel_width, screen_height, BLACK);
+
     DrawRectangle(button_x, button_y, button_width, button_height, button_color);
     DrawText(button_icon.c_str(), button_x + 10, button_y + 5, 20, button_text_color);
 
@@ -113,12 +153,20 @@ int main(void) {
                    (top_button_width - MeasureText("Open File", 20)) / 2,
                top_button_y + 10, 20, WHITE);
       // exit button
-      DrawRectangle(exit_button_x, exit_button_y, exit_button_width,
+      
+      exit_button.button_color = RED;
+      //set text settings to exit button
+      exit_button.text_color = WHITE;
+      exit_button.text = "EXIT";
+
+      exit_button.draw(1);
+      
+      /*DrawRectangle(exit_button_x, exit_button_y, exit_button_width,
                     exit_button_height, RED);
       DrawText("EXIT",
                exit_button_x +
                    (exit_button_width - MeasureText("EXIT", 20)) / 2,
-               exit_button_y + 10, 20, WHITE);
+               exit_button_y + 10, 20, WHITE);*/
     }
 
     DrawText("GUANDAO", (screen_width - MeasureText("GUANDAO", 48)) / 2,
