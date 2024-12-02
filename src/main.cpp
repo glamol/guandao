2f9e3cb6ef857bb54a07997fcca636504f66a8e3
Molphie <ei.pogr4work@gmail.com>
2024-12-03 01:23:27 +0300

    Button class reworked with usage of Rectangle structure, all panel branch was refactored to classes

--- src/main.cpp
+++ src/main.cpp
@@ -4,43 +4,76 @@
 #include <string>
 
 class Button{
-  
-  public:
-    //button
-    int x, y;
-    int width, height;
+  private:
+  //x, y, width, height
+    
     Color button_color = RAYWHITE;
     //text
     std::string text = "";
     int text_font_size = 20;
     Color text_color = BLACK;
-    
-    Button(int wid, int heig){
-      width = wid;
-      height = heig;
+
+  public:
+    Rectangle rect;
+    Button(float width, float height){
+      rect.width = width;
+      rect.height = height;
     }
-    Button(int x, int y, int wid, int heig){
-      this->x = x;
-      this->y = y;
-      width = wid;
-      height = heig;
+    Button(float x, float y, float width, float height){
+      rect.x = x;
+      rect.y = y;
+      rect.width = width;
+      rect.height = height;
     }
-    Button(int x, int y, int wid, int heig, std::string txt){
-      width = wid;
-      height = heig;
+    Button(float x, float y, float width, float height, Color color){
+      rect.x = x;
+      rect.y = y;
+      rect.width = width;
+      rect.height = height;
+      button_color = color;
+    }
+    Button(float x, float y, float width, float height, Color color, std::string txt, int font, Color txt_color){
+      rect.x = x;
+      rect.y = y;
+      rect.width = width;
+      rect.height = height;
+      button_color = color;
+
       text = txt;
+      text_font_size = font;
+      text_color = txt_color; 
     }
+    //setters
+    void setx(float x){
+      rect.x = x;
+    }
+    void sety(float y){
+      rect.y = y;
+    }
+
+    void set_color(Color color){
+      button_color = color;
+    }
+    void set_text_settings(std::string txt, Color color){
+      text = txt;
+      text_color = color;
+    }
+    void set_text_settings(std::string txt, Color color, int font){
+      text = txt;
+      text_color = color;
+      text_font_size = font;
+    }
+
     void draw(int mode){ // 0 - without text, 1 - centered text, 2 - ?? (may be added)
-      DrawRectangle(x, y, width, height, button_color);
+      DrawRectangleRec(rect, button_color);
       //or change to switch/case construction
       if (mode == 1) DrawText(text.c_str(), 
-                              x + (width - MeasureText(text.c_str(), 20)) / 2, 
-                              y + height / 4, 
+                              rect.x + (rect.width - MeasureText(text.c_str(), 20)) / 2, 
+                              rect.y + rect.height / 4, 
                               text_font_size, text_color);
     }
     bool is_clicked(Vector2 mouse_position){
-      Rectangle button_rectangle = {(float)x, (float)y, (float)width, (float)height};
-      return CheckCollisionPointRec(mouse_position, button_rectangle);
+      return CheckCollisionPointRec(mouse_position, rect);
     }
 };
 
@@ -59,21 +92,26 @@ int main(void) {
   const int button_height = 30;
   const int button_offset = 5;
   const int panel_animation_speed = 10;
+
   int panel_x = -panel_width;
   int button_x = button_offset;
   int button_y = button_offset;
+
   std::string button_icon = ">";
   CLITERAL(Color) button_color = BLACK;
   CLITERAL(Color) button_text_color = RAYWHITE;
   bool panel_visible = false;
   bool is_animating = false;
 
-  Button exit_button(panel_width - 2 * button_offset, 40);
+  Button exit_button((float)panel_width - 2 * button_offset, (float)40); 
 
-  const int top_button_width = panel_width - 2 * button_offset;
-  const int top_button_height = 40;
   const int top_button_x = button_offset;
-  const int top_button_y = button_offset + button_height + 10;
+  Button top_button(button_offset, button_offset + button_height + 10,
+                          panel_width - 2 * button_offset, 40,
+                          DARKGRAY, 
+                          "Open File", 20, WHITE);
+  Button rect_button(button_x, button_y, button_width, button_height, button_color);
+  rect_button.set_text_settings(">", RAYWHITE);
   SetTargetFPS(60);
 
   while (!WindowShouldClose()) {
@@ -81,34 +119,31 @@ int main(void) {
       button_x = panel_x + panel_width - button_width - button_offset;
     else
       button_x = button_offset;
-
-    exit_button.x = panel_x + button_offset;
-    exit_button.y = screen_height - exit_button.height - button_offset;
     
-    int actual_top_button_x = panel_x + top_button_x;
+    exit_button.rect.x = panel_x + button_offset;
+    exit_button.rect.y = screen_height - exit_button.rect.height - button_offset;
+    //exit_button.setx(panel_x + button_offset);
+    //exit_button.sety(screen_height - exit_button.height - button_offset);
 
+    top_button.rect.x = panel_x + top_button_x;
+    rect_button.rect.x = button_x;
+     
     if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
       Vector2 mouse_position = GetMousePosition();
-      Rectangle button_rect = {(float)button_x, (float)button_y,
-                               (float)button_width, (float)button_height};
-      if (CheckCollisionPointRec(mouse_position, button_rect) &&
-          !is_animating) {
+      //Rectangle button_rect = {(float)button_x, (float)button_y, (float)button_width, (float)button_height};
+      if (rect_button.is_clicked(mouse_position) && !is_animating) {
         panel_visible = !panel_visible;
         is_animating = true;
-        button_icon = panel_visible ? "<" : ">";
-        button_color = panel_visible ? RAYWHITE : BLACK;
-        button_text_color = panel_visible ? BLACK : RAYWHITE;
-
+        panel_visible ? rect_button.set_text_settings("<", BLACK) : rect_button.set_text_settings(">", RAYWHITE);
+        panel_visible ? rect_button.set_color(RAYWHITE) : rect_button.set_color(BLACK);
       }
+
       if (exit_button.is_clicked(mouse_position)) {
          CloseWindow(); 
          return 0;
       }
       
-      Rectangle top_button_rect = {(float)actual_top_button_x,
-                                   (float)top_button_y, (float)top_button_width,
-                                   (float)top_button_height};
-      if (CheckCollisionPointRec(mouse_position, top_button_rect)) {
+      if (top_button.is_clicked(mouse_position)) {
         const char *filters[] = {"*.*"};
         const char *file =
             tinyfd_openFileDialog("Select a file", "", 0, filters, NULL, 0);
@@ -138,35 +173,18 @@ int main(void) {
     BeginDrawing();
     ClearBackground(RAYWHITE);
 
-    // panel and toggle button
+    // panel
     DrawRectangle(panel_x, 0, panel_width, screen_height, BLACK);
 
-    DrawRectangle(button_x, button_y, button_width, button_height, button_color);
-    DrawText(button_icon.c_str(), button_x + 10, button_y + 5, 20, button_text_color);
-
+    rect_button.draw(1);
     if (panel_x > -panel_width + panel_animation_speed) {
       // file button
-      DrawRectangle(actual_top_button_x, top_button_y, top_button_width,
-                    top_button_height, DARKGRAY);
-      DrawText("Open File",
-               actual_top_button_x +
-                   (top_button_width - MeasureText("Open File", 20)) / 2,
-               top_button_y + 10, 20, WHITE);
+      top_button.draw(1);
+      
       // exit button
-      
-      exit_button.button_color = RED;
-      //set text settings to exit button
-      exit_button.text_color = WHITE;
-      exit_button.text = "EXIT";
-
+      exit_button.set_color(RED);
+      exit_button.set_text_settings("EXIT", WHITE);
       exit_button.draw(1);
-      
-      /*DrawRectangle(exit_button_x, exit_button_y, exit_button_width,
-                    exit_button_height, RED);
-      DrawText("EXIT",
-               exit_button_x +
-                   (exit_button_width - MeasureText("EXIT", 20)) / 2,
-               exit_button_y + 10, 20, WHITE);*/
     }
 
     DrawText("GUANDAO", (screen_width - MeasureText("GUANDAO", 48)) / 2,
